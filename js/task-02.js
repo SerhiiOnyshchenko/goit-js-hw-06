73ba1b0b77660f493ab2ccae786b50badac42024
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-01-25 20:04:55 +0200

    dz2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,9 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ul = document.querySelector('#ingredients');
+for (const el of ingredients) {
+   let li = document.createElement('li');
+   li.className = 'item';
+   li.innerHTML = el;
+   ul.append(li);
+}
