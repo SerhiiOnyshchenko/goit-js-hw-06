f580bf7929b1e601951cd2360aaeb4681d1ebadf
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-01-27 21:38:19 +0200

    dz8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const forma = document.querySelector('.login-form');
+const inputEmail = document.querySelector('input[name=email]');
+const inputPassword = document.querySelector('input[name=password]');
+const buttonSubmit = document.querySelector('button[type=submit]');
 
+buttonSubmit.addEventListener('click', event => {
+   if (inputEmail.value === '' || inputPassword.value === '') {
+      alert('Всі поля повинні бути заповнені!');
+   } else {
+      const form = {
+         email: inputEmail.value,
+         password: inputPassword.value,
+      };
+      console.log(form);
+      forma.reset();
+   }
+   event.preventDefault();
+});
