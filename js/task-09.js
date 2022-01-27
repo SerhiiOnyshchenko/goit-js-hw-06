9649a4882034a60598f7f05c9061f601b965fd4e
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-01-27 21:53:38 +0200

    dz9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyElement = document.querySelector('body');
+const btnElement = document.querySelector('.change-color');
+const spanElement = document.querySelector('span.color');
+
+btnElement.addEventListener('click', () => {
+	const color = getRandomHexColor();
+	bodyElement.style.backgroundColor = color;
+	spanElement.innerHTML = color;
+})
+
