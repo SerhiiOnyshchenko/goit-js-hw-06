f0fc9ffa7339f93b39f7cd442052ed20e0fce1a1
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-02-01 23:23:49 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,16 +1,14 @@
 const forma = document.querySelector('.login-form');
-const buttonSubmit = document.querySelector('button[type=submit]');
 
-buttonSubmit.addEventListener('click', event => {
+forma.addEventListener('submit', event => {
+   event.preventDefault();
+   const {
+      elements: { email, password },
+   } = event.currentTarget;
    if (forma.elements.email.value === '' || forma.elements.password.value === '') {
       alert('Всі поля повинні бути заповнені!');
    } else {
-      const form = {
-         email: forma.elements.email.value,
-         password: forma.elements.password.value,
-      };
-      console.log(form);
-      forma.reset();
+      console.log(`${email.name}: ${email.value}, ${password.name}: ${password.value}`);
+      event.currentTarget.reset();
    }
-   event.preventDefault();
 });
